310065c7c7033e5f4b4364a84596638ab31231a0
Somadevv <somadev@hotmail.co.uk>
2021-04-06 17:52:00 +0100

    Finished all web pages and added animated text for homepage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,47 +1,69 @@
-$(function () {
+ // TODO
+ // Optimise code & remove DRY (Do not repeat yourself)
+
+var fps = document.querySelector(".fps-container");
+var rpg = document.querySelector(".rpg-container");
+var mmorpg = document.querySelector(".mmorpg-container");
+
+// Scroll to CTA
+function scrollTo() {
+  var elmnt = document.getElementById("cta");
+  elmnt.scrollIntoView();
+}
+
+
+  // Toggle display for top rated cards
+  function toggleFps() {
+    var displaySetting = fps.style.display;
+    if (displaySetting == 'none') {
+      fps.style.display = 'block';
+      rpg.style.display = 'none';
+      mmorpg.style.display = 'none';
+    }
+    else {
+      fps.style.display = 'none';
+    }
+  }
+
+  function toggleRpg() {
+    var displaySetting = rpg.style.display;
+    if (displaySetting == 'none') {
+      rpg.style.display = 'block';
+      fps.style.display = 'none';
+      mmorpg.style.display = 'none';
+    }
+    else {
+      rpg.style.display = 'none';
+    }
+  }
+
+  function toggleMmorpg() {
+    var displaySetting = mmorpg.style.display;
+    if (displaySetting == 'none') {
+      mmorpg.style.display = 'block';
+      fps.style.display = 'none';
+      rpg.style.display = 'none';
+    }
+    else {
+      mmorpg.style.display = 'none';
+    }
+  }
+
+
+  function submitMsg() {
+    document.querySelector(".success").innerHTML = "Thanks for getting in touch!";
+  }
+  $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar-fixed-top");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
       document.querySelector(".navbar-nav").style.marginTop = "0";
       document.querySelector(".navbar-nav").style.paddingTop = "1rem";
-      
     });
     
   });
 
-  function toggleFps() {
-    var x = document.querySelector(".fps-container");
-    var displaySetting = x.style.display;
-    if (displaySetting == 'none') {
-      x.style.display = 'block';
-    }
-    else {
-      x.style.display = 'none';
 
-    }
-  }
-  function toggleRpg() {
-    var x = document.querySelector(".rpg-container");
-    var displaySetting = x.style.display;
-    if (displaySetting == 'none') {
-      x.style.display = 'block';
-    }
-    else {
-      x.style.display = 'none';
 
-    }
-  }
-
-  function toggleMmorpg() {
-    var x = document.querySelector(".mmorpg-container");
-    var displaySetting = x.style.display;
-    if (displaySetting == 'none') {
-      x.style.display = 'block';
-    }
-    else {
-      x.style.display = 'none';
-
-    }
-  }
 
 
